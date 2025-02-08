f27d989704fc125d02bd56db8a5706f4bc9f2add
Alex Javier <alexavier1989@gmail.com>
2025-02-08 13:26:09 -0500

    feat: first commit

---
+++ public/js/app.js
@@ -0,0 +1,91 @@
+
+// const authorNames = [
+//     {
+//         lowerCaseName: "gustavofring",
+//         picturePath: "/img/Gus_Fring.jpg"
+//     },{
+//         lowerCaseName: "hankschrader",
+//         picturePath: "/img/Hank_Schrader.jpg"
+//     },{
+//         lowerCaseName: "jessepinkman",
+//         picturePath: "/img/Jesse_Pinkman.jpg"
+//     },{
+//         lowerCaseName: "lalosalamanca",
+//         picturePath: "/img/Lalo_Salamanca.jpg"
+//     },{
+//         lowerCaseName: "mikeehrmantraut",
+//         picturePath: "/img/Mike_Ehrmantraut.jpg"
+//     },{
+//         lowerCaseName: "saulgoodman",
+//         picturePath: "/img/Saul_Goodman.jpg"
+//     },{
+//         lowerCaseName: "skylerwhite",
+//         picturePath: "/img/Skyler_White.jpg"
+//     },{
+//         lowerCaseName: "tiosalamanca",
+//         picturePath: "/img/Tio_Salamanca.jpg"
+//     },{
+//         lowerCaseName: "tucosalamanca",
+//         picturePath: "/img/Tuco_Salamanca.jpg"
+//     },{
+//         lowerCaseName: "walterwhite",
+//         picturePath: "/img/Walter_White.jpg"
+//     }
+
+// ]
+// function changeBackgroundByAuthor(authorResponse){
+//     const authorObj = authorNames.filter((authorName) => authorName.lowerCaseName.includes( authorResponse.toLowerCase().replace(" ","")))
+//     const imagePath = authorObj != undefined && authorObj.length > 0 ? authorObj[0].picturePath : "/img/Breaking_bad_poster.jpg";
+//     return imagePath;
+// }
+
+function callPhrasesApiByTime(apiUrl, time) {
+
+    async function fetchData() {
+        try {
+            const response = await fetch(apiUrl);
+
+            if (!response.ok) {
+                throw new Error(`Error in the response: ${response.status} - ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            let quoteElement = document.querySelector(".quote");
+            let authorElement = document.querySelector(".author");
+            let mainElement = document.querySelector(".main");
+
+            const quoteResponse = data[0].quote;
+            const authorResponse = data[0].author;
+
+            // Animation
+            quoteElement.style.transition = "opacity 0.8s";
+            authorElement.style.transition = "opacity 0.8s";
+            mainElement.style.transition = "background-image 0.8s";
+            quoteElement.style.opacity = 0;
+            authorElement.style.opacity = 0;
+
+            setTimeout(() => {
+                // let changingBackground = changeBackgroundByAuthor(authorResponse);
+                // mainElement.style.backgroundImage = `url(${changingBackground})`;
+
+                quoteElement.innerHTML = quoteResponse;
+                authorElement.innerHTML = authorResponse;
+
+                quoteElement.style.opacity = 1;
+                authorElement.style.opacity = 1;
+            }, 800);
+
+        } catch (error) {
+            console.error('API error: ', error);
+        }
+    }
+
+    fetchData();
+    setInterval(fetchData, time);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const apiUrl = "https://api.breakingbadquotes.xyz/v1/quotes";
+    callPhrasesApiByTime(apiUrl, 12000);
+})
+
---
+++ server.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const { log } = require("console");
+const exp = require("constants");
+const express = require("express");
+const path = require("path");
+const port = 1777;
+
+const app = express();
+
+app.use("", express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+   console.log(`Server is running on http://localhost:${port}`);
+   console.log("Press Ctrl + C to stop the server");
+});
+
